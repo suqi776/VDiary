dd6c20f24e72094573ca84a0ec8f5b7b25412d70
SQsuqi <643352633@qq.com>
2024-11-22 19:21:52 +0800

    ha

--- src/router/router.js
+++ src/router/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createHashHistory } from 'vue-router'
 
 const routes = [
   {
@@ -45,7 +45,7 @@ const routes = [
 ]
 
 export const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createHashHistory(),
   routes,
 })
 
